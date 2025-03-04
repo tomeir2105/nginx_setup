56aafcf645e15269f3d27fad05f1641319759c50
Meir <tomeir2105@gmail.com>
2025-03-04 18:15:50 +0200

    Create setup_nginx.sh
    startup defaults

---
+++ setup_nginx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+######################################
+# Created by : Meir
+# Purpose : Primary install script
+# Date : 14/2/25
+# Version : 1
+#set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+#####################################
+
